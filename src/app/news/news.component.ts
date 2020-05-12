62df9ef420c8f13becba59111ab38c6765b6df7e
Saikrishna26 <kvm.saikrishna@gmail.com>
2020-05-12 22:35:51 +0530

    corrections

--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import {Chart} from 'Chart.js';
 @Component({
   selector: 'app-news',
   templateUrl: './news.component.html',
